ed0c4ae359b73aa136f2820f7b28faa88b314360
Adam Samaha <adamsamahaa@gmail.com>
2016-04-15 12:17:33 -0700

    creating routes for each individual moduels

--- server/config/routeconfig.js
+++ server/config/routeconfig.js
@@ -2,6 +2,19 @@ var bodyParser = require('body-parser');
 var helpers = require('./helpers.js');
 var utility = require(__dirname + '/../utility/utility.js');
 
+module.exports = function(app, express) {
+  app.use(bodyParser.json());
+  //creating routes for each individual moduels (groups of routes)
+  
+  var userRouter = express.Router();
+  require(__dirname + '/../users/userRoutes.js')(userRouter);
+  // app.use('/api/users', utility.decode);
+  // This won't work at this point because there is no login;
+  app.use('/api/users', userRouter);
 
+  var eventRouter = express.Router();
+  require(__dirname + '/../events/eventRoutes.js')(eventRouter);
+  app.use('/api/events', utility.decode);
+  app.use('/api/events', eventRouter);
 
 };
