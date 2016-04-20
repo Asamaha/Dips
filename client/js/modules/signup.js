50651f484379ef3b7a8f0f23ae28820553d774ed
Adam Samaha <adamsamahaa@gmail.com>
2016-04-20 14:40:54 -0700

    created signup js

---
+++ client/js/modules/signup.js
@@ -0,0 +1,28 @@
+angular.module('userInfo', [])
+  .controller('userSignUp', function($scope, $window, $state, SignUpFactory) {
+    $scope.user = {};
+    $scope.user.username = '';
+    $scope.user.email = '';
+    $scope.user.password = '';
+
+    $scope.signUp = function() {
+      if($scope.user.username === '') {
+        aler("Must Enter User Name");
+      } else if($scope.user.email === '') {
+        aler("Must Enter Valid Email");
+      } else if($scope.user.password === '') {
+        aler("Must Enter Password");
+      } else {
+        $state.go('dashboardPage');	
+        SignUpFactory.signUpData($scope.user)
+          .then(function(token) {
+            $window.localStorage
+            .setItem('dibsToken', token.data);
+          });
+      }
+    },
+
+    $scope.loginPage = function() {
+      $state.go('loginupPage');      
+    };
+});
