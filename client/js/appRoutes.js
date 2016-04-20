05b266ea5e00b093c659dd42f10db16bb5e1e8c0
Adam Samaha <adamsamahaa@gmail.com>
2016-04-20 13:32:49 -0700

    pushes the result of what we have defined with factory function

--- client/js/appRoutes.js
+++ client/js/appRoutes.js
@@ -1,9 +1,21 @@
 //parent module
 // inject children modules for access
-angular.module('dibs', ['ngAnimate', 'ui.bootstrap','ui.router','eventsInfo', 'eventsInfoFactory', 'userInfo', 'userFactory', 'loginInfo', 'userloginFactory'])
+angular.module('roomsurf', ['ngAnimate', 'ui.bootstrap','ui.router','eventsInfo', 'eventsInfoFactory', 'userInfo', 'userFactory', 'loginInfo', 'userloginFactory'])
   .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
     $urlRouterProvider.otherwise('signup');
     $httpProvider.interceptors.push('AttachToken');
     //runs before reaching server and 
     //pushes the result of what we have defined with factory function
+    
+    $stateProvider 
+      .state('signupPage', {
+        url : '/signup', 
+        views: {
+          'indexPage' : {
+            templateUrl : 'views/signup.html',
+            controller : 'userSignUp'
+          }         
+        },
+        data : { authenticate: false }
+      })
 });
